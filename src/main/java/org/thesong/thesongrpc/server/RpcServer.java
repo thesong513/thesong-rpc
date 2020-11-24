cf9da7541eec583638ae89a77ead90c330eab1c2
thesong513 <30562225+thesong513@users.noreply.github.com>
2020-11-24 14:27:08 +0800

    first commit

---
+++ src/main/java/org/thesong/thesongrpc/server/RpcServer.java
@@ -0,0 +1,17 @@
+package org.thesong.thesongrpc.server;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Author thesong
+ * @Date 2020/11/24 13:57
+ * @Version 1.0
+ * @Describe
+ */
+
+@Slf4j
+public class RpcServer {
+
+
+
+}
